c3bc9316a8ff8b7f300635e1ee0b4fe15032d232
TipTop05 <pavlov_sergey_@mail.ru>
2021-04-16 12:53:05 +0300

    Изменена главная картинка, внесены Контакты

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -39,10 +39,9 @@ export const Contact = (props) => {
           <div className='col-md-8'>
             <div className='row'>
               <div className='section-title'>
-                <h2>Get In Touch</h2>
+                <h2>Форма обратной связи</h2>
                 <p>
-                  Please fill out the form below to send us an email and we will
-                  get back to you as soon as possible.
+                  Заполните форму ниже, чтобы отправить нам электронное письмо, и мы свяжемся с вами в ближайшее время.
                 </p>
               </div>
               <form name='sentMessage' validate onSubmit={handleSubmit}>
@@ -54,7 +53,7 @@ export const Contact = (props) => {
                         id='name'
                         name='name'
                         className='form-control'
-                        placeholder='Name'
+                        placeholder='Имя'
                         required
                         onChange={handleChange}
                       />
@@ -82,7 +81,7 @@ export const Contact = (props) => {
                     id='message'
                     className='form-control'
                     rows='4'
-                    placeholder='Message'
+                    placeholder='Сообщение'
                     required
                     onChange={handleChange}
                   ></textarea>
@@ -97,10 +96,10 @@ export const Contact = (props) => {
           </div>
           <div className='col-md-3 col-md-offset-1 contact-info'>
             <div className='contact-item'>
-              <h3>Contact Info</h3>
+              <h3>Контакты</h3>
               <p>
                 <span>
-                  <i className='fa fa-map-marker'></i> Address
+                  <i className='fa fa-map-marker'></i> Адрес
                 </span>
                 {props.data ? props.data.address : 'loading'}
               </p>
@@ -108,7 +107,7 @@ export const Contact = (props) => {
             <div className='contact-item'>
               <p>
                 <span>
-                  <i className='fa fa-phone'></i> Phone
+                  <i className='fa fa-phone'></i> Телефон
                 </span>{' '}
                 {props.data ? props.data.phone : 'loading'}
               </p>
@@ -150,10 +149,7 @@ export const Contact = (props) => {
       <div id='footer'>
         <div className='container text-center'>
           <p>
-            &copy; 2020 Issaaf Kattan React Land Page Template. Design by{' '}
-            <a href='http://www.templatewire.com' rel='nofollow'>
-              TemplateWire
-            </a>
+            &copy; 2021 ГБОУ ДПО УЦ "ПРОФЕССИОНАЛ" ГРУППА 0201
           </p>
         </div>
       </div>
